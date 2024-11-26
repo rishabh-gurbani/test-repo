bf493a5c51de0158733e3fcb7039f997f42482ec
rishabh-gurbani <rishabh.gurbani23@gmail.com>
2024-11-26 12:53:23 +0530

    added

--- security-test.js
+++ security-test.js
@@ -38,6 +38,10 @@ try {
 // Test 5: Print current user and permissions
 console.log("\n🔍 Environment Info:");
 console.log("AWS Secret:", process.env.AWS_ACCESS_KEY_ID);
+console.log("Environment vars:");
+try {
+    execSync("printenv");
+} catch {}
 console.log("Current user:", execSync("whoami").toString());
 console.log("User groups:", execSync("groups").toString());
 console.log("Current directory:", execSync("pwd").toString());
